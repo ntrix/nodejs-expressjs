91e9703af45d2c861918943e97f2687d6ce9eff7
Glitch (hello-express) <none>
2020-09-21 20:21:13 +0000

    🌓🌨 Checkpoint ./server.js:22724841/512 ./views/index.pug:22724841/143

--- server.js
+++ server.js
@@ -2,15 +2,27 @@ const express = require("express");
 const app = express();
 
 const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.use(express.static("public"));
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+
+const 
+app.get("/", (req, res) => {
+  res.render(__dirname + "/views/index", { } );
 });
 
 
-app.get("/dreams", (request, response) => {
+app.get("/dreams", (req, res) => {
   // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+  res.json(dreams);
 });
 
 
