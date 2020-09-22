35a2f2c6cc2b2a585fa209f7b446c6e0f7b917c5
Glitch (scythe-phrygian-talk) <none>
2020-09-22 21:31:59 +0000

    🌵📫 Checkpoint ./views/transactions/index.pug:22724841/255 ./routes/tran.route.js:22724841/2242 ./shared/db.json:22724841/17 ./views/transactions/create.pug:22724841/242

--- routes/tran.route.js
+++ routes/tran.route.js
@@ -3,38 +3,24 @@ const router = express.Router();
 const shortid = require('shortid');
 
 const db = require('../shared/db');
-const trans = db.get('transactions').value();
-
-router.post('/add', (req, res) => {
-  if (req.body.title.length){
-    req.body.id = shortid.generate();
-    db.get('transactions').push(req.body).write();
-    res.redirect('back');
-  }
-})
+const trans = db.get('trans').value();
+const users = db.get('users').value();
+const books = db.get('books').value();
 
 router.get('/', (req, res) => {
-  res.render("transactions/index", { transactions: trans });
+  res.render("trans/index", { trans: trans });
 });
 
-router.get('/upd/:id', (req, res) => {
-  res.render('transactions/create', {
-    transactions: trans,
-    chosenTrans: trans.find(b => b.id === req.params.id)
-  });
-})
+router.get('/create', (req, res) => {
+  res.render("trans/create", { users: users, books: books });
+});
 
-router.post('/upd', (req, res) => {
-  console.log(req)
-  db.get('transactions').find({ id: req.body.id })
-    .assign(req.body)
-    .write();
-  res.redirect(req.baseUrl);
-})
-
-router.get('/del/:id', (req, res) => {
-  db.get('transactions').remove({ id: req.params.id }).write();
-  res.redirect(req.baseUrl);
+router.post('/create', (req, res) => {
+  if (req.body.title.length){
+    req.body.id = shortid.generate();
+    db.get('trans').push(req.body).write();
+    res.redirect('back');
+  }
 })
 
 module.exports = router;
