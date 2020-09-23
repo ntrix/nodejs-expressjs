76903ffcd2f1f1e0713d45a832af53188d806425
Glitch (scythe-phrygian-talk) <none>
2020-09-23 19:41:37 +0000

    ✨🎑 Checkpoint ./validate/user.validate.js:22724841/365

--- validate/user.validate.js
+++ validate/user.validate.js
@@ -1,5 +1,9 @@
-module.exports.postAdd = (req, res, next) => {
-  if (!req.body.username || req.body.username.length > 30)
-    res.redirect(req.baseUrl + '/error');
-  next();
+module.exports = {
+  errors: [],
+  postAdd: (req, res, next) => {
+    if (req.body.username && req.body.username.length <= 30)
+      next();
+    this.errors.push('Username must have at leat one and maximal 30 charaters');
+    res.redirect(req.baseUrl);
+  }
 }
