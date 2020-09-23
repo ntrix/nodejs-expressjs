7f5a806eeaa69ec751f897249aa28a99d08e64f9
Glitch (scythe-phrygian-talk) <none>
2020-09-23 18:56:07 +0000

    🍢🌄 Checkpoint ./controller/tran.controller.js:22724841/593 ./views/trans/index.pug:22724841/12

--- controller/tran.controller.js
+++ controller/tran.controller.js
@@ -8,25 +8,17 @@ const books = db.get('books').value();
 module.exports = {
 
   index: (req, res) => {
-    var joinTrans = trans.map(t => {
-      {
-        id: t.id,
-        title: db.get('books').find({ id: t.bookId }).value().title,
-        username: db.get('users').find({ id: t.userId }).value().username,
-        isComplete: t.isComplete
-      }
-    });
-    res.render("trans/index", { trans: joinTrans });
+    var transList = trans.map(t => ({
+      id: t.id,
+      title: db.get('books').find({ id: t.bookId }).value().title,
+      username: db.get('users').find({ id: t.userId }).value().username,
+      isComplete: t.isComplete
+    }) );
+    res.render("trans/index", { trans: transList });
   },
   
   create: (req, res) => {
     res.render("trans/create", { users: users, books: books });
-  },
-  
-  postCreate: (req, res) => {
-    req.body.id = 't' + shortid.generate();
-    db.get('trans').push(req.body).write();
-    res.redirect(req.baseUrl);
   },
   
   complete: (req, res) => {
@@ -35,5 +27,11 @@ module.exports = {
         res.send(err); //render error page
     }).set('isComplete', true).write();
     res.redirect('back');
-  }  
+  },
+  
+  postCreate: (req, res) => {
+    req.body.id = 't' + shortid.generate();
+    db.get('trans').push(req.body).write();
+    res.redirect(req.baseUrl);
+  }
 }
