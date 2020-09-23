dd8236e0b30a1ad09f74d5628b73c6bdf96f1392
Glitch (scythe-phrygian-talk) <none>
2020-09-23 19:30:06 +0000

    👟🍮 Checkpoint ./validate/user.validate.js:22724841/153

--- validate/user.validate.js
+++ validate/user.validate.js
@@ -1,4 +1,5 @@
 module.exports.postAdd = (req, res, next) => {
-  if (!req.body.username)
+  if (!req.body.username || req.body.username.length > 30)
+    res.redirect(req.baseUrl + '/error');
   next();
 }
