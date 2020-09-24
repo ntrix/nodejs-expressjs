889d513a2e4e2f5eb3e6e734325b58a11babe91b
Glitch (scythe-phrygian-talk) <none>
2020-09-24 13:02:36 +0000

    👾🗼 Checkpoint ./server.js:22724841/113

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
-const shortid = require('shortid');
+//const shortid = require('shortid');
+//const userId = 'temp'+shortid.generate();
 
 const userRoute = require('./routes/user.route');
 const bookRoute = require('./routes/book.route');
@@ -11,7 +12,6 @@ const authRoute = require('./routes/auth.route');
 
 const app = express();
 
-const userId = 'temp'+shortid.generate();
 let cookieCount = 0;
 
 app.set('view engine', 'pug');
@@ -21,7 +21,8 @@ app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use(express.static("public"));
 
-app.use(cookieParser() /*, (req, res, next) => {
+app.use(cookieParser()
+/*, (req, res, next) => {
   if (!req.cookies['user-id']) {
     res.cookie('user-id', userId);
     console.log('No cookie found, set cookie, cookie count: ', cookieCount = 0);
@@ -29,7 +30,7 @@ app.use(cookieParser() /*, (req, res, next) => {
     console.log(req.cookies, ': ', ++cookieCount)
   next();
 }*/
-       )
+)
 app.get("/", (req, res) => {
   res.render("index");
 });
