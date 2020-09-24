f69708aca2b1ceba4f8cd6cc56ebce14abbcad14
Glitch (scythe-phrygian-talk) <none>
2020-09-24 23:43:24 +0200

    config after pull from glitch

--- controller/tran.controller.js
+++ controller/tran.controller.js
@@ -1,44 +1,41 @@
-const shortid = require('shortid');
+const shortid = require("shortid");
 
-const db = require('../shared/db');
-const users = db.get('users').value();
-const books = db.get('books').value();
+const db = require("../shared/db");
+const users = db.get("users").value();
+const books = db.get("books").value();
 
 module.exports = {
+	index: (req, res) => {
+		const id = req.signedCookies.userId;
+		const isAdmin = +req.signedCookies.isAdmin;
+		let trans = db.get("trans").value();
 
-  index: (req, res) => {
-    const id = req.signedCookies.userId;
-    const isAdmin = +req.signedCookies.isAdmin;
-    console.log(typeof id, typeof isAdmin);
-    let trans = db.get('trans').value();
-    
-    if (!isAdmin)
-      trans = trans.filter(t => t.userId === id );
-    
-    let transList = trans.map(t => ({
-      id: t.id,
-      title: db.get('books').find({ id: t.bookId }).value().title,
-      username: db.get('users').find({ id: t.userId }).value().username,
-      isComplete: t.isComplete
-    }) );
-    res.render("trans/index", { trans: transList, isAdmin: isAdmin});
-  },
-  
-  create: (req, res) => {
-    res.render("trans/create", { users: users, books: books });
-  },
-  
-  complete: (req, res) => {
-    const matchedTran = db.get('trans').find({ id: req.params.id });
-    if (!matchedTran.value()) res.send('Transaction(id) does not exist'); // or render error page
-    
-    matchedTran.set('isComplete', true).write();
-    res.redirect('back');
-  },
-  
-  postCreate: (req, res) => {
-    req.body.id = 't' + shortid.generate();
-    db.get('trans').push(req.body).write();
-    res.redirect(req.baseUrl);
-  }
-}
+		if (!isAdmin) trans = trans.filter((t) => t.userId === id);
+
+		let transList = trans.map((t) => ({
+			id: t.id,
+			title: db.get("books").find({ id: t.bookId }).value().title,
+			username: db.get("users").find({ id: t.userId }).value().username,
+			isComplete: t.isComplete,
+		}));
+		res.render("trans/index", { trans: transList, isAdmin: isAdmin });
+	},
+
+	create: (req, res) => {
+		res.render("trans/create", { users: users, books: books });
+	},
+
+	complete: (req, res) => {
+		const matchedTran = db.get("trans").find({ id: req.params.id });
+		if (!matchedTran.value()) res.send("Transaction(id) does not exist"); // or TODO render error page
+
+		matchedTran.set("isComplete", true).write();
+		res.redirect("back");
+	},
+
+	postCreate: (req, res) => {
+		req.body.id = "t" + shortid.generate();
+		db.get("trans").push(req.body).write();
+		res.redirect(req.baseUrl);
+	},
+};
